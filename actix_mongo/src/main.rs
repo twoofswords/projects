e7b746ecbca444041f1ffdc7246739e92b02eba3
Carmen DeCastro <43592660+twoofswords@users.noreply.github.com>
2020-12-10 21:12:43 -0600

    Replace code in main.rs to use actix_web

--- actix_mongo/src/main.rs
+++ actix_mongo/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+use std::env;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+async fn hello() -> impl Responder {
+    format!("Hello fellow Rustacean!")
 }
